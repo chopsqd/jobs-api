708ff239b056ad6a3501094787a9a0e11b3eec09
Michael Chop <007micle007@gmail.com>
2023-02-20 23:52:53 +0300

    Finished with the main jobs controllers

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,64 @@
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
 const getAllJobs = async (req, res) => {
-    res.send('getAllJobs')
+    const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-    res.send('getJob')
+    const { user: {userId}, params: {id: jobId} } = req
+
+    const job = await Job.findOne({ _id: jobId, createdBy: userId })
+    if(!job) {
+        throw new NotFoundError(`Not found job with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req, res) => {
-    res.send('createJob')
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req, res) => {
-    res.send('updateJob')
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId },
+    } = req
+
+    if (company === '' || position === '') {
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-    res.send('deleteJob')
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req
+
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId,
+    })
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
