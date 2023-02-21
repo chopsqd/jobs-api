87b570c2be9a7632491eb61b269b717d9e6f4761
Michael Chop <007micle007@gmail.com>
2023-02-21 11:21:44 +0300

    Added Swagger documentation to the API

--- app.js
+++ app.js
@@ -8,10 +8,36 @@ const jobsRouter = require('./routes/jobs')
 const authenticateUser = require('./middleware/authentication')
 const mongoose = require("mongoose");
 
+// Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors())
+app.use(xss())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
