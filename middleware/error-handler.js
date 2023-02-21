8ea6a415ac4fa0c288b750797dcbc12ff344a479
Michael Chop <007micle007@gmail.com>
2023-02-21 10:53:06 +0300

    Refactored some error handlers

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,33 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message })
+    let customError = {
+        // Set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        message: err.message || 'Something went wrong try again later'
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message })
+    // }
+
+    if(err.name === 'ValidationError') {
+        customError.message = Object.values(err.errors)
+            .map(item => item.message)
+            .join(',')
+        customError.statusCode = 400
+    }
+
+    if(err.code && err.code === 11000) {
+        customError.message = `Duplicate value entered to ${Object.keys(err.keyValue)} field, please choose another value`
+        customError.statusCode = 400
+    }
+
+    if(err.name === 'CastError') {
+        customError.message = `No item found with id: ${err.value}`
+        customError.statusCode = 404
+    }
+
+    return res.status(customError.statusCode).json({ message: customError.message })
 }
 
 module.exports = errorHandlerMiddleware
